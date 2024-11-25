85a6d335b4323a8e60f6de6c6bd1547e669c6ed1
Adam Mertzenich <adam+git@mertzeni.ch>
2024-11-25 12:20:14 -0600

    Add floating RuneLite launcher rules

--- config.h
+++ config.h
@@ -50,14 +50,16 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	*/
 	/* class    instance      title       	 tags mask    isfloating   isterminal  noswallow  monitor */
-	{ "Gimp",     NULL,       NULL,       	    1 << 8,       0,           0,         0,        -1 },
-	{ TERMCLASS,  NULL,       NULL,       	    0,            0,           1,         0,        -1 },
-	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
-	{ NULL,       NULL,       "Minecraft 24w18a",   0,            1,           0,         1,        -1 },
-	{ TERMCLASS,      "floatterm", NULL,       	    0,       1,           1,         0,        -1 },
-	{ TERMCLASS,      "bg",        NULL,       	    1 << 7,       0,           1,         0,        -1 },
-	{ TERMCLASS,      "spterm",    NULL,       	    SPTAG(0),     1,           1,         0,        -1 },
-	{ TERMCLASS,      "spcalc",    NULL,       	    SPTAG(1),     1,           1,         0,        -1 },
+	{ "Gimp",         NULL,        NULL,       	        1 << 8,       0,           0,         0,        -1 },
+	{ TERMCLASS,      NULL,        NULL,       	        0,            0,           1,         0,        -1 },
+	{ NULL,           NULL,        "Event Tester",       0,            0,           0,         1,        -1 },
+	{ NULL,           NULL,        "Minecraft 24w18a",   0,            1,           0,         1,        -1 },
+	{ "net-runelite-launcher-Launcher",           NULL,        NULL,	                0,            1,           0,         1,        -1 },
+	{ "net-runelite-client-RuneLite",           NULL,        "RuneLite Launcher",	                0,            1,           0,         1,        -1 },
+	{ TERMCLASS,      "floatterm", NULL,            0,            1,           1,         0,        -1 },
+	{ TERMCLASS,      "bg",        NULL,            1 << 7,       0,           1,         0,        -1 },
+	{ TERMCLASS,      "spterm",    NULL,            SPTAG(0),     1,           1,         0,        -1 },
+	{ TERMCLASS,      "spcalc",    NULL,            SPTAG(1),     1,           1,         0,        -1 },
 };
 
 /* layout(s) */
